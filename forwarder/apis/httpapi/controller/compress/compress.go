e6ffdd08dc851a643c4c9d97efcc064b8979d13a
woodenstone <1145230019@qq.com>
2023-02-16 00:50:06 +0800

    feat(forwarder): adapt api interface

--- forwarder/apis/httpapi/controller/compress/compress.go
+++ forwarder/apis/httpapi/controller/compress/compress.go
@@ -1,19 +1,21 @@
 package compress
 
 import (
+	"encoding/json"
 	customerror "forwarder/internal/custom-error"
 	"forwarder/internal/model"
+	"mime/multipart"
 
 	"github.com/gin-gonic/gin"
 )
 
 type CompressOneImageRequest struct {
-	FileName string `form:"filename" binding:"required"`
-	Model    string `form:"model" binding:"required"`
-	Filetype string `form:"filetype" binding:"required"`
-	Metric   string `form:"metric" binding:"required"`
-	Quality  int    `form:"quality" binding:"required"`
-	File     []byte `form:"file" binding:"required"`
+	FileName string                `form:"filename" binding:"required"`
+	Model    string                `form:"model" binding:"required"`
+	Filetype string                `form:"filetype" binding:"required"`
+	Metric   string                `form:"metric" binding:"required"`
+	Quality  int                   `form:"quality" binding:"required"`
+	File     *multipart.FileHeader `form:"file" binding:"required"`
 }
 
 type CompressOneImageResponse struct {
@@ -22,15 +24,21 @@ type CompressOneImageResponse struct {
 }
 
 func CompressOneImage(c *gin.Context) (any, *customerror.CError) {
-	// extract request form data and bind to struct
+	// extract request from multipart/formdata and bind to struct
 	var req CompressOneImageRequest
 	if err := c.ShouldBind(&req); err != nil {
 		return nil, customerror.GenInvalidParamError(err)
 	}
 
+	// convert multipart.FileHeader to []byte
+	fileBytes, err := convertFileToByte(req.File)
+	if err != nil {
+		return nil, customerror.GenCompressFailedError(err)
+	}
+
 	// call model to compress image
 	response, err := model.CompressImage(&model.CompressRPCRequest{
-		Image:     req.File,
+		Image:     fileBytes,
 		FileName:  req.FileName,
 		FileType:  req.Filetype,
 		ModelName: req.Model,
@@ -42,10 +50,18 @@ func CompressOneImage(c *gin.Context) (any, *customerror.CError) {
 		return nil, customerror.GenCompressFailedError(err)
 	}
 
-	return &CompressOneImageResponse{
-		Metrics: response.Metrics,
-		Data:    response.CompressedFile,
-	}, nil
+	metricStr, err := json.Marshal(response.Metrics)
+	if err != nil {
+		return nil, customerror.GenCompressFailedError(err)
+	}
+
+	// add metrics to response header's X-Metrics
+	c.Writer.Header().Set("X-Metrics", string(metricStr))
+
+	// add the file to response body
+	c.Data(200, "application/octet-stream", response.CompressedFile)
+
+	return nil, nil
 }
 
 type AutoCompressOneImageRequest struct {
@@ -53,8 +69,8 @@ type AutoCompressOneImageRequest struct {
 	Filetype string `form:"filetype" binding:"required"`
 	Metric   string `form:"metric" binding:"required"`
 	// Quality: low, medium or high, not required
-	Quality string `form:"quality"`
-	File    []byte `form:"file" binding:"required"`
+	Quality string                `form:"quality"`
+	File    *multipart.FileHeader `form:"file" binding:"required"`
 }
 
 type AutoCompressOneImageResponse struct {
@@ -62,6 +78,7 @@ type AutoCompressOneImageResponse struct {
 	Data    []byte            `json:"data"`
 }
 
+// TODO 更新File 为*multipart.FileHeader
 func AutoCompressOneImage(c *gin.Context) (any, *customerror.CError) {
 	// extract request form data and bind to struct
 	var req AutoCompressOneImageRequest
@@ -69,9 +86,14 @@ func AutoCompressOneImage(c *gin.Context) (any, *customerror.CError) {
 		return nil, customerror.GenInvalidParamError(err)
 	}
 
+	fileBytes, err := convertFileToByte(req.File)
+	if err != nil {
+		return nil, customerror.GenCompressFailedError(err)
+	}
+
 	// call model to classify image
 	classifyResp, err := model.ClassifyImage(&model.ClassifyRPCRequest{
-		Image: req.File,
+		Image: fileBytes,
 	})
 
 	if err != nil {
@@ -85,7 +107,7 @@ func AutoCompressOneImage(c *gin.Context) (any, *customerror.CError) {
 
 	// call model to compress image
 	compressResp, err := model.CompressImage(&model.CompressRPCRequest{
-		Image:     req.File,
+		Image:     fileBytes,
 		FileName:  req.FileName,
 		FileType:  req.Filetype,
 		ModelName: modelName,
@@ -97,8 +119,16 @@ func AutoCompressOneImage(c *gin.Context) (any, *customerror.CError) {
 		return nil, customerror.GenCompressFailedError(err)
 	}
 
-	return &AutoCompressOneImageResponse{
-		Metrics: compressResp.Metrics,
-		Data:    compressResp.CompressedFile,
-	}, nil
+	metricStr, err := json.Marshal(compressResp.Metrics)
+	if err != nil {
+		return nil, customerror.GenCompressFailedError(err)
+	}
+
+	// add metrics to response header's X-Metrics
+	c.Writer.Header().Set("X-Metrics", string(metricStr))
+
+	// add the file to response body
+	c.Data(200, "application/octet-stream", compressResp.CompressedFile)
+
+	return nil, nil
 }
--- forwarder/apis/httpapi/controller/compress/parser.go
+++ forwarder/apis/httpapi/controller/compress/parser.go
@@ -1,20 +1,43 @@
 package compress
 
-import "forwarder/internal/config"
+import (
+	"bytes"
+	"forwarder/internal/config"
+	"io"
+	"mime/multipart"
+)
+
+const (
+	qualityLabel   = "qualityLabel"
+	defaultQuality = "auto"
+)
 
 // getModelNameByLabel returns the model name and quality for the given label, metric and quality.
-// If quality is specified, the model name will be decided by the given quality
+// If quality is specified, the model name will be decided by the given quality automatically.
 func getModelNameByLabel(label, metric, quality string) (autoQuality, modelName string) {
 	sc := config.GlobalStrategyConfig
 	mn := sc.CompressionStrategy[label][metric]
-	if quality != "" {
+	if quality != "" && quality != defaultQuality {
 		return quality, mn
 	}
-	return sc.CompressionStrategy[label][quality], mn
+	return sc.CompressionStrategy[label][qualityLabel], mn
 }
 
 // transformQuality returns the quality for the given quality and model name.
 func transformQuality(quality, modelName string) int {
 	sc := config.GlobalStrategyConfig
 	return sc.QualityMap[quality][modelName]
+}
+
+func convertFileToByte(file *multipart.FileHeader) ([]byte, error) {
+	f, err := file.Open()
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	fileBytes := bytes.NewBuffer(nil)
+	if _, err = io.Copy(fileBytes, f); err != nil {
+		return nil, err
+	}
+	return fileBytes.Bytes(), nil
 }
